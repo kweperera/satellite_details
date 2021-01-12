c41410d94ffeb2f021c072cb922d7cd49c682047
unknown <kweperera93111@gmail.com>
2021-01-12 14:01:27 +0530

    initial commit

---
+++ satellite_catalog/src/test/java/com/satellite_api_master/satellite_catalog/SatelliteCatalogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.satellite_api_master.satellite_catalog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SatelliteCatalogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
